1dfbf71287269147a4fc3d69a1348f7e10868083
Diane Ujeneza <dianesis17@gmail.com>
2019-08-30 16:40:02 +0200

    created repository component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule} from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SearchFormComponent } from './search-form/search-form.component';
 import { UserComponent } from './user/user.component';
+import { HttpClientModule } from '@angular/common/http';
+import { RepositoryComponent } from './repository/repository.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SearchFormComponent,
-    UserComponent
+    UserComponent,
+    RepositoryComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/request.service.ts
+++ src/app/request.service.ts
@@ -10,32 +10,34 @@ export class RequestService {
 	user: User;
 
 	constructor(private http: HttpClient) {
-		this.user = new User('', '', '', 0);
+		this.user = new User('', '', '', 0,new Date());
 	}
 	githubRequest() {
 		interface ApiResponse {
 			login: string;
 			name: string;
 			avatar_url: string;
-			public_repos: number;
+      public_repos: number;
+      created_at:Date;
 		}
 		let promise = new Promise((resolve, reject) => {
 			this.http
-				.get<ApiResponse>('https://api.github.com/users/ahiodette?access_token=' + environment.key)
+				.get<ApiResponse>('https://api.github.com/users/dianeujeneza?access_token=' + environment.key)
 				.toPromise()
 				.then(
 					(response) => {
 						this.user.login = response.login;
 						this.user.name = response.name;
 						this.user.avatar_url = response.avatar_url;
-						this.user.public_repos = response.public_repos;
+            this.user.public_repos = response.public_repos;
+            this.user.created_at = response.created_at;
 						resolve();
 					},
 					(error) => {
 						this.user.login = 'User not found';
 						this.user.name = 'No name is found';
 						this.user.avatar_url = 'No profile picture';
-						this.user.public_repos = 0;
+            this.user.public_repos = 0;
 						reject(error);
 					}
 				);
